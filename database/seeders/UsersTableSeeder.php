9666a9c72152b1a83af09135ddc091c5212c54dd
ssalpo <ssalpomishevich@gmail.com>
2023-02-23 16:49:08 +0500

    split production and local environment users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,35 +17,44 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'name' => 'User 1',
-                'username' => 'user1',
-                'password' => 'secret',
-                'role' => 'admin'
-            ],
-            [
-                'name' => 'User 2',
-                'username' => 'user2',
-                'password' => 'secret',
-                'role' => 'resident'
-            ],
-            [
-                'name' => 'User 3',
-                'username' => 'user3',
-                'password' => 'secret',
-                'role' => 'doctor'
-            ],
-            [
-                'name' => 'User 4',
-                'username' => 'user4',
-                'password' => 'secret',
-                'role' => 'doctor'
-            ],
-        ];
+        $users = app()->isLocal()
+            ? [
+                [
+                    'name' => 'User 1',
+                    'username' => 'user1',
+                    'password' => 'secret',
+                    'role' => 'admin'
+                ],
+                [
+                    'name' => 'User 2',
+                    'username' => 'user2',
+                    'password' => 'secret',
+                    'role' => 'resident'
+                ],
+                [
+                    'name' => 'User 3',
+                    'username' => 'user3',
+                    'password' => 'secret',
+                    'role' => 'doctor'
+                ],
+                [
+                    'name' => 'User 4',
+                    'username' => 'user4',
+                    'password' => 'secret',
+                    'role' => 'doctor'
+                ],
+            ]
+            : [
+                [
+                    'name' => 'Султонов Рустам Алпомишевич',
+                    'username' => 'rustam',
+                    'password' => 'secret',
+                    'role' => 'admin'
+                ]
+            ];
 
         foreach ($users as $userData) {
-            $user = User::create(array_merge(Arr::except($userData, 'role') , ['password' => $userData['password']]));
+            $user = User::create(array_merge(Arr::except($userData, 'role'), ['password' => $userData['password']]));
 
             $user->assignRole($userData['role']);
         }
